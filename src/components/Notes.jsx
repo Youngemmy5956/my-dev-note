1548950a55dd88076eadd13107bef3ca0eb50e73
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-06-03 08:09:09 +0100

    new

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+
 import Note from "./Note";
+import React, {useState} from "react";
+import ReactPaginate from "react-paginate";
 
 export default function Notes({ notes, onDelete, onEdit }) {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const PER_PAGE = 3;
+  const pageCount = Math.ceil(notes.length / PER_PAGE);
+
+  function handlePageclick({selected: selectedPage}) {
+    setCurrentPage(selectedPage);
+  }
+  const offset = currentPage * PER_PAGE;
   return (
+    <>
     <div className="flex flex-col ">
       {notes.map((item) => (
         <Note key={item.id} note={item} onDelete={onDelete} onEdit={onEdit} />
       ))}{" "}
     </div>
+
+    <ReactPaginate 
+    breakLabel="..."
+        previousLabel="< previous"
+        nextLabel="next >"
+        pageCount={pageCount}
+        onPageChange={handlePageclick}
+        pageRangeDisplayed={5}
+        renderOnZeroPageCount={null}
+        className="flex justify-between my-4"
+        />
+
+
+    </>
   );
 }
